a479ba6004089520ea5226d3084fd639102e5e32
Florian Delearde <fdeleard@z4r11p7.42lyon.fr>
2026-04-27 13:17:19 +0200

    fixed port not being initialized

--- include/config/ServerConfig.hpp
+++ include/config/ServerConfig.hpp
@@ -18,7 +18,7 @@ struct	ServerConfig
 
 	ServerConfig(void) : port(80), client_max_body_size(1048576) {}
 
-	void	setPort(std::string new_port) { new_port = std::atoi(new_port.c_str()); }
+	void	setPort(std::string new_port) { port = std::atoi(new_port.c_str()); }
 	void	setServerName(std::string new_server_name) { server_name = new_server_name; }
 	void	setClientMaxBodySize(std::string new_client_mbs) { client_max_body_size = static_cast<size_t>(std::atoi(new_client_mbs.c_str())); }
 	void	setErrorPages(std::string new_code, std::string new_page) { int	code = std::atoi(new_code.c_str()); error_pages[code] = new_page; }
